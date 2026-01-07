91451c156a628e1a0f0e0371458de98f0992d6f8
Gollumkles <julian.gantschacher@gmail.com>
2026-01-07 11:28:20 +0100

    stedenten Package erstellen + java class

---
+++ src/main/java/at/itkollegimst/studentenverwaltung/domain/student.java
@@ -0,0 +1,29 @@
+package at.itkollegimst.studentenverwaltung.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Size(min = 3)
+    private String name;
+
+    @Size(min=4, max = 6)
+    private String plz;
+}
