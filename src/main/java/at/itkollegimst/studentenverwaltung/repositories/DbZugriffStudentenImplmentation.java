1420bfca8d079c76d2de6b4d36176a282a202a27
Gollumkles <julian.gantschacher@gmail.com>
2026-01-13 21:29:57 +0100

    Implementieren javaclasse dbzugriff mit autowired

---
+++ src/main/java/at/itkollegimst/studentenverwaltung/repositories/DbZugriffStudentenImplmentation.java
@@ -0,0 +1,37 @@
+package at.itkollegimst.studentenverwaltung.repositories;
+
+import at.itkollegimst.studentenverwaltung.domain.student;
+import at.itkollegimst.studentenverwaltung.exceptions.StudentNichtGefunden;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class DbZugriffStudentenImplmentation implements DbZugriffStudenten  {
+
+    @Autowired
+    private  StudentJPARepo studentJPARepo;
+
+
+
+    @Override
+    public student StudentSpeicher(student Student) {
+        return null;
+    }
+
+    @Override
+    public List<student> alleStudenten() {
+        return List.of();
+    }
+
+    @Override
+    public student studentMitId(Long id) throws StudentNichtGefunden {
+        return null;
+    }
+
+    @Override
+    public void studentLoeschenMitId(Long id) {
+
+    }
+}
